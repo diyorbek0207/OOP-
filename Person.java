fc4ae3eec27e06fcd229d35da51c2e8886822d08
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2024-02-28 11:56:39 +0500

    add person

---
+++ Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    private String ismi;
+    private String familiyasi;
+    private int yoshi;
+
+    public String getIsmi() {
+        return ismi;
+    }
+
+    public void setIsmi(String ismi) {
+        this.ismi = ismi;
+    }
+
+    public String getFamiliyasi() {
+        return familiyasi;
+    }
+
+    public void setFamiliyasi(String familiyasi) {
+        this.familiyasi = familiyasi;
+    }
+
+    public int getYoshi() {
+        return yoshi;
+    }
+
+    public void setYoshi(int yoshi) {
+        this.yoshi = yoshi;
+    }
+}
