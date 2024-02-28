f0cd7dfb554520e225908f792666aa85cfaf578d
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2024-02-28 11:55:57 +0500

    add car

---
+++ Car.java
@@ -0,0 +1,38 @@
+public class Car {
+    private String color;
+    private String modeli;
+    private int prise;
+    private String tizim;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModeli() {
+        return modeli;
+    }
+
+    public void setModeli(String modeli) {
+        this.modeli = modeli;
+    }
+
+    public int getPrise() {
+        return prise;
+    }
+
+    public void setPrise(int prise) {
+        this.prise = prise;
+    }
+
+    public String getTizim() {
+        return tizim;
+    }
+
+    public void setTizim(String tizim) {
+        this.tizim = tizim;
+    }
+}
